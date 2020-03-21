6e75f36b87973ed2ea4733d31981ea779aa30078
goosung <iibanf69@gmail.com>
2020-03-21 15:01:35 -0700

    changed videos to channels

--- channel/urls.py
+++ channel/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.video_list, name='video_list'),
+    path('', views.channel_list, name='channel_list'),
 ]
--- channel/views.py
+++ channel/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Channel
 
-def video_list(request):
-    return render(request, 'channel/video_list.html', {})
+def channel_list(request):
+    channels = Channel.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'channel/channel_list.html', {'channels': channels})
