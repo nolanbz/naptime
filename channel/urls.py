1787fb41e9a723e09e2a047dafc4adc24aed676b
goosung <iibanf69@gmail.com>
2020-03-21 18:00:56 -0700

    Added view and template for detailed channel as well as CSS for the site.

--- channel/urls.py
+++ channel/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.channel_list, name='channel_list'),
+    path('channel/<int:pk>/', views.channel_detail, name='channel_detail'),
 ]
--- channel/views.py
+++ channel/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Channel
 
 def channel_list(request):
     channels = Channel.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'channel/channel_list.html', {'channels': channels})
+
+def channel_detail(request, pk):
+    channel = get_object_or_404(Channel, pk=pk)
+    return render(request, 'channel/channel_detail.html', {'channel': channel})
